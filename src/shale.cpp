508a43587df75dd1b6b457452accb29e3248bc06
Graeme Elsworthy <graeme@sharkshead.com>
2023-05-25 22:40:54 +1000

    Update copyright date.

--- src/shale.cpp
+++ src/shale.cpp
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2020-2021 Graeme Elsworthy <github@sharkshead.com>
+Copyright (c) 2020-2023 Graeme Elsworthy <github@sharkshead.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
